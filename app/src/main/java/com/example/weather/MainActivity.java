f42531ad624a9ec1e711994ac2270d74f1101f8f
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-01-02 21:57:10 +0500

    Project Configurations

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
