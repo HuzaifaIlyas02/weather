cdb7cc0086a1b8a7626a766627010c6c45912c6f
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-01-04 09:21:17 +0500

    Added the animation XML files

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.weather;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.check_weather.R;
 
 public class MainActivity extends AppCompatActivity {
 
